515f134590f658cf9177d658904e2e2f99df2bcd
axe-rigged <alex20101@student.hamk.fi>
2022-04-14 18:58:48 +0000

    commit message

---
+++ DjangoX/blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+#Alemmasta saadaan authorit
+from django.conf import settings
+
+#Tarvittavat pip kirjastot PIL, django-stdimage
+#from PIL import Image
+# from stdimage.validators import MinSizeValidator
+#from stdimage import StdimageField, JPEGField
+# Create your models here.
